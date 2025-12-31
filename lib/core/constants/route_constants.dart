736848c2b18cbd58b077be1b82459ccb3eab0e77
Nnaemeka  Kingsley Ugwumba <jolicity2021@gmail.com>
2025-12-31 03:12:48 +0100

    Add RouteConstants class for application routes

---
+++ lib/core/constants/route_constants.dart
@@ -0,0 +1,10 @@
+abstract class RouteConstants {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+  static const String products = '/products';
+  static const String productDetail = '/product/:id';
+  static const String settings = '/settings';
+  static const String language = '/language';
+}
